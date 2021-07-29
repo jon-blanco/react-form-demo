b69b2da11b6b33639ee1e78b43f3248f993b4660
jonblanco-gif <76707900+jonblanco-gif@users.noreply.github.com>
2021-07-28 22:40:52 -0400

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, TextInput, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function App () {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [petName, setPetName] = useState('')
+  const [petDOB, setPetDOB] = useState('')
+  const [breed, setBreed] = useState('')
+  const [toy, setToy] = useState('')
+
+  return (<View style={{ flex: 1, justifyContent: 'center', backgroundColor: '#ecf0f1', padding:16 }}>
+
+  <Input label={'Email'} placeholder={'you@website.com'} value={email} callBack={setEmail}/>
+  <Input label={'Password'} secureTextEntry={true}  placeholder={'shh... it\'s a secret'} value={password} callBack={setPassword}/>
+  <Input label={'Pet\'s Name'} placeholder={'Mx. Snuffles'} value={petName} callBack={setPetName}/>
+  <Input label={'Pet\s Date of Birth'} placeholder={'MM/DD/YYYY'} value={petDOB} callBack={setPetDOB}/>
+  <Input label={'Breed'} placeholder={'It\'s a mystery... or is it?'} value={breed} callBack={setBreed}/>
+  <Input label={'Favorite Toy'} placeholder={'Stick'} value={toy} callBack={setToy}/>
+  </View>)
+};
+
+export default App;
+
+export const Input = (props) => (
+  <View >
+  <Text style={{padding: 8, fontSize: 16}}>{props.label}</Text>
+  <TextInput
+        style={{padding: 8, fontSize: 16, height: 40, borderColor: "grey", borderRadius: 4}}
+        secureTextEntry={props.label === 'Password'}
+        placeholder={props.placeholder}
+        onChangeText={() => {props.callBack(props.value)}}
+
+      />
+  </View>
+)
